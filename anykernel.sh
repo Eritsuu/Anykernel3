bfd80a575622e1c1ee04c9caef9e295332455655
Eritsuu96 <133488662+Eritsuu@users.noreply.github.com>
2024-06-13 19:45:19 +0700

    update

--- anykernel.sh
+++ anykernel.sh
@@ -1,103 +1,88 @@
-### AnyKernel3 Ramdisk Mod Script
-## osm0sis @ xda-developers
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
 
-### AnyKernel setup
-# global properties
+## AnyKernel setup
+# begin properties
 properties() { '
-kernel.string=Custom Kernel by Eritsuu @ Telegram
+kernel.string=Erinayo Kernel V1.0.0 - by @Eritsuu
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=olives
+device.name1=pine
 device.name2=olive
-device.name3=mi439
-supported.versions=
+device.name3=olivelite
+device.name4=olivewood
+device.name5=mi439
+device.name6=olives
+device.name7=Mi439
+supported.versions=11,12,13
 supported.patchlevels=
-supported.vendorpatchlevels=
 '; } # end properties
 
-
-### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-} # end attributes
-
-# boot shell variables
+# shell variables
 block=/dev/block/by-name/boot;
-is_slot_device=1;
+is_slot_device=0;
 ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+# R/W dynamic partitions fix
+if [ -d "/dev/block/mapper" ]; then
+    blockdev --setrw /dev/block/mapper/system
+    blockdev --setrw /dev/block/mapper/vendor
+fi
 
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-## end boot install
+## AnyKernel install
+split_boot;
 
+ui_print "Mounting /vendor..."
+mount -o rw,remount /vendor
 
-## init_boot files attributes
-#init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $ramdisk/*;
-#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-#} # end attributes
+vndk_version=$(file_getprop /vendor/build.prop ro.vendor.build.version.sdk)
 
-# init_boot shell variables
-#block=init_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
+if [ $vndk_version -lt 30 ]; then
+    abort "Unsupported VNDK version. Aborting..."
+fi
 
-# reset for init_boot patching
-#reset_ak;
+ui_print "Detecting WiFi HAL..."
 
-# init_boot install
-#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
+wifi_hal=$(find /vendor/lib64 /vendor/lib -name "libwifi-hal.so" | head -n 1)
+if grep -q "pronto_wlan.ko" $wifi_hal; then
+    abort "Unsupported WiFi HAL. Aborting..."
+fi
 
-#write_boot;
-## end init_boot install
+ui_print "Detecting camera HAL..."
 
+if [[ ! -z "$(find /vendor/lib64 /vendor/lib -name '*lib2d*')" ]]; then
+    patch_cmdline "prebuilt_camera_hal" "prebuilt_camera_hal"
+else
+    patch_cmdline "prebuilt_camera_hal" ""
+fi
 
-## vendor_kernel_boot shell variables
-#block=vendor_kernel_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
+flash_boot;
+flash_dtbo;
 
-# reset for vendor_kernel_boot patching
-#reset_ak;
+ui_print " "
 
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
+ui_print "Mounting /system..."
+mount -o rw,remount /system
 
-#flash_boot;
-## end vendor_kernel_boot install
-
-
-## vendor_boot files attributes
-#vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $ramdisk/*;
-#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-#} # end attributes
-
-# vendor_boot shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-# vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-## end vendor_boot install
-
+## Patches
+# Prevent init from overriding kernel tweaks.
+ui_print "Patching system's init..."
+# IMO this is kinda destructive but works
+find /system/etc/init/ -type f | while read file; do 
+sed -Ei 's;[^#](write /proc/sys/(kernel|vm)/(sched|dirty|perf_cpu|page-cluster|stat|swappiness|vfs));#\1;g' $file
+done
+# Prevent init from overriding ZRAM algorithm
+ui_print "Patching vendor's init..."
+remove_line "/vendor/etc/init/hw/init.qcom.rc" "comp_algorithm" "global"
+# Replace post_boot with ours.
+ui_print "Pushing init.qcom.post_boot.sh..."
+replace_file "/vendor/bin/init.qcom.post_boot.sh" "0755" "init.qcom.post_boot.sh"
+## end install
